b9c6f9b1beb439df5f14697bf30c4a560e248f72
doganciftcir6 <doganciftcir6@hotmail.com>
2023-03-09 11:04:25 +0300

    EditTopBar

--- Project.AndoridIosApp.UI/Areas/Admin/ViewComponents/AdminTopBarComponent.cs
+++ Project.AndoridIosApp.UI/Areas/Admin/ViewComponents/AdminTopBarComponent.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.AndroidIosApp.Business.Abstract.Services;
+using System.Threading.Tasks;
 
 namespace Project.AndoridIosApp.UI.Areas.Admin.ViewComponents
 {
     public class AdminTopBarComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IProjectUserService _projectUserService;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public AdminTopBarComponent(IProjectUserService projectUserService, IHttpContextAccessor contextAccessor)
         {
+            _projectUserService = projectUserService;
+            _contextAccessor = contextAccessor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //login olmuş kişiyi bulmak
+            var loginUserName = _contextAccessor.HttpContext.User.Identity.Name;
+            var loginUser = await _projectUserService.FindByUserNameAsync(loginUserName);
+            if(loginUser.ResponseType == AndroidIosApp.Core.Enums.ResponseType.Success)
+            {
+               return View(loginUser.Data);
+            }
             return View();
         }
     }

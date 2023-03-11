b50e449ce2cedbf9e3a95cdde05c0f0976f099d0
doganciftcir6 <doganciftcir6@hotmail.com>
2023-03-11 22:36:15 +0300

    AddAdminGenderPage

---
+++ Project.AndoridIosApp.UI/Areas/Admin/Controllers/AdminGenderController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.AndroidIosApp.Business.Abstract.Services;
+using Project.AndroidIosApp.Dtos.GenderDto;
+using System.Threading.Tasks;
+
+namespace Project.AndoridIosApp.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminGender/{action}/{id?}")]
+    public class AdminGenderController : Controller
+    {
+        private readonly IGenderService _genderService;
+
+        public AdminGenderController(IGenderService genderService)
+        {
+            _genderService = genderService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _genderService.GetAllAsync();
+            return View(response.Data);
+        }
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Insert(CreateGenderDto createGenderDto)
+        {
+            var response = await _genderService.InsertAsync(createGenderDto);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            else if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.ValidationError)
+            {
+                foreach (var item in response.ValidationErrors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(createGenderDto);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _genderService.GetByIdAsync<UpdateGenderDto>(id);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            return View(response.Data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateGenderDto updateGenderDto)
+        {
+            var response = await _genderService.UpdateAsync(updateGenderDto);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            else if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.ValidationError)
+            {
+                foreach (var item in response.ValidationErrors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(updateGenderDto);
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _genderService.DeleteAsync(id);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

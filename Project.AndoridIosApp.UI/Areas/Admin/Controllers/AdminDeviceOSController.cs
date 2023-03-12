c34294d44c066d028b989a3a4de2d1789b57464e
doganciftcir6 <doganciftcir6@hotmail.com>
2023-03-12 15:08:55 +0300

    AddAdminDeviceOSPage

---
+++ Project.AndoridIosApp.UI/Areas/Admin/Controllers/AdminDeviceOSController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.AndroidIosApp.Business.Abstract.Services;
+using Project.AndroidIosApp.Dtos.OSDtos;
+using System.Threading.Tasks;
+
+namespace Project.AndoridIosApp.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminDeviceOS/{action}/{id?}")]
+    public class AdminDeviceOSController : Controller
+    {
+        private readonly IOSService _oSService;
+
+        public AdminDeviceOSController(IOSService oSService)
+        {
+            _oSService = oSService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _oSService.GetAllAsync();
+            return View(response.Data);
+        }
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Insert(CreateOSDto createOSDto)
+        {
+            var response = await _oSService.InsertAsync(createOSDto);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            else if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.ValidationError)
+            {
+                foreach (var item in response.ValidationErrors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(createOSDto);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var response = await _oSService.GetByIdAsync<UpdateOSDto>(id);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            return View(response.Data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateOSDto updateOSDto)
+        {
+            var response = await _oSService.UpdateAsync(updateOSDto);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            else if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.ValidationError)
+            {
+                foreach (var item in response.ValidationErrors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View(updateOSDto);
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _oSService.DeleteAsync(id);
+            if(response.ResponseType == AndroidIosApp.Core.Enums.ResponseType.NotFound)
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
